03b334e8d75e3c74a8b91696916c0e11546447d1
Kirk P Smith <7277901+kirkpsmith@users.noreply.github.com>
2019-05-28 18:39:20 -0500

    added sample data from voltcycle, see kirk.ipynb

--- voltammetry/plotting.py
+++ voltammetry/plotting.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import altair as alt
+alt.data_transformers.enable('default', max_rows=None)
 def plot_voltammogram(t, i, v, convention = 'IUPAC'):
     """ Convenience function for plotting voltammograms
 
